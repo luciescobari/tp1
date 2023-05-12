56164d3ff71ac57279a7535fa48d729c309fc0b9
luciescobari <luciana.escobari@davinci.edu.ar>
2023-05-12 12:45:27 -0300

    Ejercicio 2

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -1,27 +1,15 @@
 "use strict";
 
 let productos = [];
+let codigo =0, nombre='', categoria='', precio=0, descripcion='';
 
-function agregarProducto () {
+const cargarProductos = () => {
 
-    do {
-        let codigo = parseInt (prompt("Ingrese el código del producto:"));
-        let nombre = prompt ("Ingrese el nombre del producto:");
-        let categoria = prompt ("Ingrese la categoría del producto:");
-        let precio = parseFloat (prompt("Ingrese el precio del producto:"));
-        let descripcion = prompt ("Ingrese la descripción del producto:");
-    } while (confirm ("¿Desea seguir ingresando productos?"));
-
-    let producto = {
-        codigo: codigo,
-        nombre: nombre,
-        categoria: categoria,
-        precio: precio,
-        descripcion: descripcion,
-    }
-
-    productos.push (producto);
-    alert ("Se cargó el producto");
+    codigo = document.getElementById ('codigoProducto').value;
+    nombre = document.getElementById ('nombreProducto').value;
+    categoria = document.getElementById ('categoriaProducto').value;
+    precio = document.getElementById ('precioProducto').value;
+    descripcion = document.getElementById ('descripcionProducto').value;
     
 }
 
